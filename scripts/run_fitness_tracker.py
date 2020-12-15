41aff97926e50bb9a9573daed96d3d0633886878
guillaumebrg <gberger.eclille@gmail.com>
2020-12-14 21:25:53 -0500

    Use new loading features in run_fitness_tracker

--- scripts/run_fitness_tracker.py
+++ scripts/run_fitness_tracker.py
@@ -8,30 +8,44 @@ Usage:
                          [--path_in=FILENAME]
                          [--path_out=FILENAME]
                          [--title=TITLE]
+                         [--model_name=NAME]
+                         [--model_version=VERSION]
                          [--use_gpu]
   run_fitness_tracker.py (-h | --help)
 
 Options:
-  --weight=WEIGHT        Weight (in kilograms). Will be used to convert predicted MET value to calories [default: 70]
-  --age=AGE              Age (in years). Will be used to convert predicted MET value to calories [default: 30]
-  --height=HEIGHT        Height (in centimeters). Will be used to convert predicted MET value to calories [default: 170]
-  --gender=GENDER        Gender ("male" or "female" or "other"). Will be used to convert predicted MET value to calories
-  --path_in=FILENAME     Video file to stream from
-  --path_out=FILENAME    Video file to stream to
-  --title=TITLE          This adds a title to the window display
+  --weight=WEIGHT            Weight (in kilograms). Will be used to convert predicted MET value to calories [default: 70]
+  --age=AGE                  Age (in years). Will be used to convert predicted MET value to calories [default: 30]
+  --height=HEIGHT            Height (in centimeters). Will be used to convert predicted MET value to calories [default: 170]
+  --gender=GENDER            Gender ("male" or "female" or "other"). Will be used to convert predicted MET value to calories
+  --path_in=FILENAME         Video file to stream from
+  --path_out=FILENAME        Video file to stream to
+  --title=TITLE              This adds a title to the window display
+  --model_name=NAME          Name of the model to be used.
+  --model_version=VERSION    Version of the model to be used.
+  --use_gpu                  Whether to run inference on the GPU or not.
 """
-import torch
 from docopt import docopt
 
 import sense.display
-from sense import engine
 from sense import feature_extractors
+from sense.loading import ModelConfig, get_relevant_weights
 from sense.controller import Controller
 from sense.downstream_tasks import calorie_estimation
 from sense.downstream_tasks.fitness_activity_recognition import INT2LAB
 from sense.downstream_tasks.nn_utils import LogisticRegression
 from sense.downstream_tasks.nn_utils import Pipe
 from sense.downstream_tasks.postprocess import PostprocessClassificationOutput
+
+
+REQUIRED_FEATURE_CONVERTERS = ['fitness_activity_recognition', 'met_converter']
+
+SUPPORTED_MODEL_CONFIGURATIONS = [
+    ModelConfig('StridedInflatedMobileNetV2', 'pro', REQUIRED_FEATURE_CONVERTERS),
+    ModelConfig('StridedInflatedEfficientNet', 'pro', REQUIRED_FEATURE_CONVERTERS),
+    ModelConfig('StridedInflatedMobileNetV2', 'lite', REQUIRED_FEATURE_CONVERTERS),
+    ModelConfig('StridedInflatedEfficientNet', 'lite', REQUIRED_FEATURE_CONVERTERS),
+]
 
 
 if __name__ == "__main__":
@@ -46,24 +60,30 @@ if __name__ == "__main__":
     path_out = args['--path_out'] or None
     title = args['--title'] or None
     use_gpu = args['--use_gpu']
+    model_name = args['--model_name'] or None
+    model_version = args['--model_version'] or None
 
-    # Load feature extractor
-    feature_extractor = feature_extractors.StridedInflatedMobileNetV2()
-    checkpoint = engine.load_weights('resources/backbone/strided_inflated_mobilenet.ckpt')
-    feature_extractor.load_state_dict(checkpoint)
+    # Load weights
+    selected_config, weights = get_relevant_weights(
+        SUPPORTED_MODEL_CONFIGURATIONS,
+        model_name,
+        model_version
+    )
+
+    # Create feature extractor
+    feature_extractor = getattr(feature_extractors, selected_config.model_name)()
+    feature_extractor.load_state_dict(weights['backbone'])
     feature_extractor.eval()
 
-    # Load fitness activity classifier
+    # Create fitness activity classifier
     gesture_classifier = LogisticRegression(num_in=feature_extractor.feature_dim,
                                             num_out=81)
-    checkpoint = engine.load_weights('resources/fitness_activity_recognition/mobilenet_logistic_regression.ckpt')
-    gesture_classifier.load_state_dict(checkpoint)
+    gesture_classifier.load_state_dict(weights['fitness_activity_recognition'])
     gesture_classifier.eval()
 
-    # Load MET value converter
+    # Create MET value converter
     met_value_converter = calorie_estimation.METValueMLPConverter()
-    checkpoint = torch.load('resources/calorie_estimation/mobilenet_features_met_converter.ckpt')
-    met_value_converter.load_state_dict(checkpoint)
+    met_value_converter.load_state_dict(weights['met_converter'])
     met_value_converter.eval()
 
     # Concatenate feature extractor with downstream nets
