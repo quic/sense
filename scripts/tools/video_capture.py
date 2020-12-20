87f871da49e2803a9344ed5b82afbfbf5096a102
Manik Dhingra <manik.dhingra311@gmail.com>
2020-12-20 02:05:19 -0500

    updated audio files

--- scripts/tools/video_capture.py
+++ scripts/tools/video_capture.py
@@ -31,8 +31,8 @@ from threading import Thread
 
 FONT = cv2.FONT_HERSHEY_PLAIN
 _shutdown = False
-countdown_sound = 'done-beep.wav'
-done_sound = 'beep.wav'
+countdown_sound = 'counter_sound.wav'
+done_sound = 'long_done_sound.wav'
 
 
 class ShutDownWatcher(Thread):
