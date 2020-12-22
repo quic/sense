b4d6122e2e97a1716e8eab0ec70e2ffefcd9e42d
Sunny Panchal <sunny.panchal@twentybn.com>
2020-12-22 15:49:02 -0500

    renamed variables

--- scripts/tools/annotation/annotation.py
+++ scripts/tools/annotation/annotation.py
@@ -36,109 +36,130 @@ app = Flask(__name__)
 app.secret_key = 'd66HR8dç"f_-àgjYYic*dh'
 
 
-def extension_ok(nomfic):
+def extension_ok(filename):
     """ Returns `True` if the file has a valid image extension. """
-    return '.' in nomfic and nomfic.rsplit('.', 1)[1] in ('png', 'jpg', 'jpeg', 'gif', 'bmp')
+    return '.' in filename and filename.rsplit('.', 1)[1] in ('png', 'jpg', 'jpeg', 'gif', 'bmp')
 
 
-@app.route('/annot/')
-def list_annot():
+@app.route('/annotate/')
+def prepare_gridview():
     """Gets the data and creates the HTML template with all videos for the given class-label."""
     folder_id = zip(videos, list(range(len(videos))))
     return render_template('up_folder.html', folders=folder_id)
 
 
-@app.route('/annot/<nom>')
-def annot(nom):
+@app.route('/annotate/<idx>')
+def annotate(idx):
     """For the given class-label, this shows all the frames for annotating the selected video."""
-    nom = int(nom)
-    features = np.load(join(features_dir, videos[nom] + ".npy"))
+    idx = int(idx)
+    features = np.load(join(features_dir, videos[idx] + ".npy"))
     features = features.mean(axis=(2, 3))
 
-    if lr is not None:
-        classes = list(lr.predict(features))
+    if logreg is not None:
+        classes = list(logreg.predict(features))
     else:
         classes = [0] * len(features)
-    print(classes)
 
     # The list of images in the folder
-    images = [img for img in glob.glob(join(frames_dir, videos[nom] + '/*')) if extension_ok(img)]
+    images = [image for image in glob.glob(join(frames_dir, videos[idx] + '/*'))
+              if extension_ok(image)]
 
-    nums = [int(x.split('.')[0].split('/')[-1]) for x in images]
-    n_images = len(nums)
-    images = [[x.replace(frames_dir, ''), y] for y, x in sorted(zip(nums, images))]
-    images = [[x[0], x[1], y] for x, y in zip(images, classes)]
+    indexes = [int(image.split('.')[0].split('/')[-1]) for image in images]
+    n_images = len(indexes)
+
+    images = [[image.replace(frames_dir, ''), ii] for ii, image in sorted(zip(indexes, images))]
+    images = [[image[0], image[1], _class] for image, _class in zip(images, classes)]
+
     chunk_size = 5
     images = np.array_split(images, np.arange(chunk_size, len(images), chunk_size))
-    images = [list(x) for x in images]
-    print(f"Number of images: {n_images}")
-    print(images)
-    return render_template('up_list.html', images=images, num=nom, fps=16, n_images=n_images, video_name=videos[nom])
+    images = [list(image) for image in images]
+
+    return render_template('up_list.html', images=images, idx=idx, fps=16,
+                           n_images=n_images, video_name=videos[idx])
 
 
 @app.route('/response', methods=['POST'])
 def response():
     if request.method == 'POST':
         data = request.form  # a multi-dict containing POST data
-        num = int(data['num'])
+        print(data)
+        idx = int(data['idx'])
         fps = float(data['fps'])
-        next_frame_num = num + 1
-        description = {'file': videos[num] + ".mp4", 'fps': fps}
-        out_annotation = os.path.join(tags_dir, videos[num] + ".json")
+        next_frame_idx = idx + 1
+
+        description = {'file': videos[idx] + ".mp4", 'fps': fps}
+
+        out_annotation = os.path.join(tags_dir, videos[idx] + ".json")
         time_annotation = []
+
         for i in range(int(data['n_images'])):
             time_annotation.append(int(data[str(i)]))
+
         description['time_annotation'] = time_annotation
         json.dump(description, open(out_annotation, 'w'))
-        if next_frame_num >= len(videos):
-            return redirect(url_for('list_annot'))
-    return redirect(url_for('annot', nom=next_frame_num))
+
+        if next_frame_idx >= len(videos):
+            return redirect(url_for('prepare_gridview'))
+
+    return redirect(url_for('annotate', idx=next_frame_idx))
 
 
-@app.route('/train_lr', methods=['POST'])
-def train_lr():
-    global lr
+@app.route('/train_logreg', methods=['POST'])
+def train_logreg():
+    global logreg
+
     if request.method == 'POST':
         data = request.form  # a multi-dict containing POST data
-        num = int(data['num'])
+        idx = int(data['idx'])
         annotations = os.listdir(tags_dir)
         class_weight = {0: 0.5}
+
         if annotations:
             features = [join(features_dir, x.replace('.json', '.npy')) for x in annotations]
             annotations = [join(tags_dir, x) for x in annotations]
             X = []
             y = []
+
             for feature in features:
                 feature = np.load(feature)
+
                 for f in feature:
                     X.append(f.mean(axis=(1, 2)))
+
             for annotation in annotations:
                 annotation = json.load(open(annotation, 'r'))['time_annotation']
                 pos1 = np.where(np.array(annotation).astype(int) == 1)[0]
+
                 if len(pos1) > 0:
                     class_weight.update({1: 2})
+
                     for p in pos1:
                         try:
                             annotation[p + 1] = 1
                         except:
                             1
+
                 pos1 = np.where(np.array(annotation).astype(int) == 2)[0]
+
                 if len(pos1) > 0:
                     class_weight.update({2: 2})
+
                     for p in pos1:
                         try:
                             annotation[p + 1] = 2
                         except:
                             1
+
                 for a in annotation:
                     y.append(a)
+
             X = np.array(X)
-            print(X.shape)
             y = np.array(y)
-            lr = LogisticRegression(C=0.1, class_weight=class_weight)
-            lr.fit(X, y)
-            dump(lr, lr_path)
-    return redirect(url_for('annot', nom=num))
+            logreg = LogisticRegression(C=0.1, class_weight=class_weight)
+            logreg.fit(X, y)
+            dump(logreg, logreg_path)
+
+    return redirect(url_for('annotate', idx=idx))
 
 
 @app.after_request
@@ -170,16 +191,16 @@ if __name__ == '__main__':
     features_dir = dataset_path + f"features_{split}/{label}/"
     frames_dir = dataset_path + f"frames_{split}/{label}/"
     tags_dir = dataset_path + f"tags_{split}/{label}/"
-    lr_dir = join(dataset_path, 'lr', label)
-    os.makedirs(lr_dir, exist_ok=True)
+    logreg_dir = join(dataset_path, 'logreg', label)
+    os.makedirs(logreg_dir, exist_ok=True)
     os.makedirs(tags_dir, exist_ok=True)
 
     videos = os.listdir(frames_dir)
     videos.sort()
 
-    lr = None
-    lr_path = join(lr_dir, 'lr.joblib')
-    if os.path.isfile(lr_path):
-        lr = load(lr_path)
+    logreg = None
+    logreg_path = join(logreg_dir, 'logreg.joblib')
+    if os.path.isfile(logreg_path):
+        logreg = load(logreg_path)
 
     app.run(debug=True)
