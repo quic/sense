d49a454c61cecb2f21097210f94bb8f50e04e514
corneliusboehm <cornelius.boehm@twentybn.com>
2021-01-07 22:05:04 +0100

    Prefill classes and add path suggestions

--- scripts/tools/annotation/static/main.js
+++ scripts/tools/annotation/static/main.js
@@ -1,5 +1,63 @@
 
-function addClassInput() {
+function prefillClasses() {
+    var path = document.getElementById('path').value;
+    var xhttp = new XMLHttpRequest();
+
+    let json = JSON.stringify({path: path});
+
+    xhttp.open("POST", "/check-existing-project");
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhttp.responseType = 'json';
+
+    xhttp.send(json);
+
+    xhttp.onload = function() {
+        let response = xhttp.response;
+        pathExists = response.path_exists;
+        classes = response.classes;
+        subdirs = response.subdirs;
+
+        console.log(pathExists, classes, subdirs);
+
+        var shouldCreateDirectory = document.getElementById('shouldCreateDirectory')
+        var pathSuggestions = document.getElementById('pathSuggestions');
+        var classList = document.getElementById('classList');
+        var createProject = document.getElementById('createProject');
+
+        while (pathSuggestions.firstChild) {
+           pathSuggestions.removeChild(pathSuggestions.lastChild);
+        }
+
+        for (const subdir of subdirs) {
+            var option = document.createElement('option');
+            option.value = subdir;
+            pathSuggestions.appendChild(option);
+        }
+
+        while (classList.firstChild) {
+            classList.removeChild(classList.lastChild);
+        }
+
+        if (path && (pathExists || shouldCreateDirectory.checked)) {
+            for (const className of classes){
+                addClassInput(className);
+            }
+            addClassInput("");
+            createProject.disabled = false;
+        } else {
+            createProject.disabled = true;
+        }
+
+        if (pathExists) {
+            shouldCreateDirectory.disabled = true;
+        } else {
+            shouldCreateDirectory.disabled = false;
+        }
+    };
+}
+
+
+function addClassInput(className) {
     var classList = document.getElementById('classList');
     var numClasses = classList.children.length;
 
@@ -9,7 +67,8 @@ function addClassInput() {
     var classInput = document.createElement('input');
     classInput.type = 'text';
     classInput.name = 'class' + numClasses;
-    classInput.setAttribute('onclick', 'addClassInput();');
+    classInput.value = className;
+    classInput.setAttribute('onfocus', 'addClassInput("");');
 
     var labelInput0 = document.createElement('input');
     labelInput0.type = 'text';
@@ -33,6 +92,8 @@ function addClassInput() {
     classList.appendChild(node);
 
     // Remove onclick handler on previous node
-    var previousNode = classList.children[numClasses - 1].children[0];
-    previousNode.removeAttribute('onclick');
+    if (numClasses > 0) {
+        var previousNode = classList.children[numClasses - 1].children[0];
+        previousNode.removeAttribute('onfocus');
+    }
 }
