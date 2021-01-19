5e2c79b4da39cb8735153ca8dda2b24d2f8bb5bb
Yashesh Savani <yashesh.savani@twentybn.com>
2021-01-19 11:21:08 -0400

    shell script added for demo testing

---
+++ tests/test_demo_scripts.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# Sense SHORTCUTS
+# ------------------------------------------------------------------------------
+shopt -s expand_aliases
+# Demo scripts test run for each files
+alias demo_calorie_estimation='PYTHONPATH=./ python scripts/run_calorie_estimation.py --weight=65 --age=30 --height=170 --gender=female --path_in=tests/resources/test_video.mp4'
+
+alias demo_fitness_rep_counter='PYTHONPATH=./ python scripts/run_fitness_rep_counter.py --path_in=tests/resources/test_video.mp4'
+
+alias demo_fitness_tracker='PYTHONPATH=./ python scripts/run_fitness_tracker.py --weight=65 --age=30 --height=170 --gender=female --path_in=tests/resources/test_video.mp4'
+
+alias demo_gesture_recognition='PYTHONPATH=./ python scripts/run_gesture_recognition.py --path_in=tests/resources/test_video.mp4'
+
+# ------------------------------------------------------------------------------
+# CHAINED COMMANDS
+# ------------------------------------------------------------------------------
+
+# Run all demo files at once
+test_demos() {
+    echo "Test calorie estimation demo ::"
+    demo_calorie_estimation
+
+    echo "Test Fitness Rep Counter demo ::"
+    demo_fitness_rep_counter
+
+    echo "Test fitness tracker demo ::"
+    demo_fitness_tracker
+
+    echo "Test gesture recognition demo ::"
+    demo_gesture_recognition
+}
+
+test_demos
+echo "All tests passed!"
