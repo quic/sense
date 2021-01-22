06e7fc2661c24aeea210ff4a04c011273e86bd7c
Yashesh Savani <yashesh.savani@twentybn.com>
2021-01-22 13:55:47 -0400

    changed to cv2.KEEPRATIO

--- sense/display.py
+++ sense/display.py
@@ -291,7 +291,7 @@ class DisplayResults:
             Thickness of the display border.
         """
         self._window_title = 'Real-time SenseNet'
-        cv2.namedWindow(self._window_title, cv2.WINDOW_GUI_NORMAL + cv2.WINDOW_FREERATIO)
+        cv2.namedWindow(self._window_title, cv2.WINDOW_GUI_NORMAL + cv2.WINDOW_KEEPRATIO)
         cv2.resizeWindow(self._window_title, 640, 480)
         self.title = title
         self.display_ops = display_ops
