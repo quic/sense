09b52dece023d2f8a3ac88a9b92bc7b3c71dc28e
corneliusboehm <cornelius.boehm@twentybn.com>
2021-02-03 08:23:24 +0100

    Extract method for parsing class name and tags

--- tools/sense_studio/sense_studio.py
+++ tools/sense_studio/sense_studio.py
@@ -272,6 +272,22 @@ def project_details(path):
     return render_template('project_details.html', config=config, path=path, stats=stats)
 
 
+def _get_class_name_and_tags(form_data):
+    """
+    Extract 'className', 'tag1' and 'tag2' from the given form data and make sure that the tags
+    are not empty or the same.
+    """
+    class_name = form_data['className']
+    tag1 = form_data['tag1'] or f'{class_name}_tag1'
+    tag2 = form_data['tag2'] or f'{class_name}_tag2'
+
+    if tag2 == tag1:
+        tag1 = f'{tag1}_1'
+        tag2 = f'{tag2}_2'
+
+    return class_name, tag1, tag2
+
+
 @app.route('/add-class/<string:project>', methods=['POST'])
 def add_class(project):
     """
@@ -284,14 +300,7 @@ def add_class(project):
     path = projects[project]['path']
 
     # Get class name and tags
-    data = request.form
-    class_name = data['className']
-    tag1 = data['tag1'] or f'{class_name}_tag1'
-    tag2 = data['tag2'] or f'{class_name}_tag2'
-
-    if tag2 == tag1:
-        tag1 = f'{tag1}_1'
-        tag2 = f'{tag2}_2'
+    class_name, tag1, tag2 = _get_class_name_and_tags(request.form)
 
     # Update project config
     config = _load_project_config(path)
@@ -322,14 +331,7 @@ def edit_class(project, class_name):
     path = projects[project]['path']
 
     # Get new class name and tags
-    data = request.form
-    new_class_name = data['className']
-    new_tag1 = data['tag1'] or f'{new_class_name}_tag1'
-    new_tag2 = data['tag2'] or f'{new_class_name}_tag2'
-
-    if new_tag2 == new_tag1:
-        new_tag1 = f'{new_tag1}_1'
-        new_tag2 = f'{new_tag2}_2'
+    new_class_name, new_tag1, new_tag2 = _get_class_name_and_tags(request.form)
 
     # Update project config
     config = _load_project_config(path)
