2d0bce6ecb19e0eb9516a114c90de65b64142ed6
guillaumebrg <gberger.eclille@gmail.com>
2021-02-09 15:17:04 -0500

    Fix unittests

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -1,20 +1,20 @@
 import unittest
 
-import sense.downstream_tasks.nn_utils as nn_utils
+import sense.loading as loading
 from sense import RESOURCES_DIR
 
 
 class TestLoadWeightsFromResources(unittest.TestCase):
 
-    RELATIVE_PATH = 'gesture_detection/efficientnet_logistic_regression.ckpt'
+    RELATIVE_PATH = 'gesture_recognition/strided_inflated_efficientnet_lite_logistic_regression.ckpt'
     ABSOLUTE_PATH = '{}/{}'.format(RESOURCES_DIR, RELATIVE_PATH)
 
     def test_load_weights_from_resources_on_relative_path(self):
-        _ = nn_utils.load_weights_from_resources(self.RELATIVE_PATH)
+        _ = loading.load_weights_from_resources(self.RELATIVE_PATH)
 
     def test_load_weights_from_resources_on_absolute_path(self):
-        _ = nn_utils.load_weights_from_resources(self.ABSOLUTE_PATH)
+        _ = loading.load_weights_from_resources(self.ABSOLUTE_PATH)
 
     def test_load_weights_from_resources_on_wrong_path(self):
         wrong_path = 'this/path/does/not/exist'
-        self.assertRaises(FileNotFoundError, nn_utils.load_weights_from_resources, wrong_path)
+        self.assertRaises(FileNotFoundError, loading.load_weights_from_resources, wrong_path)
