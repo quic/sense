07e1268a8f685a3a65c5486cf845079ca87f1eca
guillaumebrg <gberger.eclille@gmail.com>
2021-02-16 14:45:17 -0500

    Fix integration tests

--- tests/scripts/test_examples.sh
+++ tests/scripts/test_examples.sh
@@ -6,15 +6,15 @@ calorie_estimation_args="--weight=65 --age=30 --height=170 --gender=female --pat
 
 echo "Test Gesture Recognition demo ::"
 PYTHONPATH=./ python examples/run_gesture_recognition.py --path_in=$test_video_path
-PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args --model_version=lite
-PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args --model_name=StridedInflatedMobileNetV2
-PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args --model_name=StridedInflatedMobileNetV2 --model_version=lite
+PYTHONPATH=./ python examples/run_gesture_recognition.py $calorie_estimation_args --model_version=lite
+PYTHONPATH=./ python examples/run_gesture_recognition.py $calorie_estimation_args --model_name=StridedInflatedMobileNetV2
+PYTHONPATH=./ python examples/run_gesture_recognition.py $calorie_estimation_args --model_name=StridedInflatedMobileNetV2 --model_version=lite
 
 echo "Test Fitness Tracker demo ::"
 PYTHONPATH=./ python examples/run_fitness_tracker.py $calorie_estimation_args
-PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args --model_version=lite
-PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args --model_name=StridedInflatedEfficientNet
-PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args --model_name=StridedInflatedEfficientNet --model_version=lite
+PYTHONPATH=./ python examples/run_fitness_tracker.py $calorie_estimation_args --model_version=lite
+PYTHONPATH=./ python examples/run_fitness_tracker.py $calorie_estimation_args --model_name=StridedInflatedEfficientNet
+PYTHONPATH=./ python examples/run_fitness_tracker.py $calorie_estimation_args --model_name=StridedInflatedEfficientNet --model_version=lite
 
 echo "Test Calorie Estimation demo ::"
 PYTHONPATH=./ python examples/run_calorie_estimation.py $calorie_estimation_args
