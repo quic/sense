28fc64d4e1cba6c30999eaea0a5e776146da94bf
Yashesh Savani <yashesh.savani@twentybn.com>
2021-02-17 13:10:54 -0400

    record videos script created

---
+++ tools/sense_studio/video_recorder/record_videos.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import subprocess
+import urllib
+
+from flask import Blueprint
+from flask import jsonify
+from flask import render_template
+from flask import request
+
+from tools.sense_studio.utils import _lookup_project_path
+
+video_recorder_bp = Blueprint('video_recorder_bp', __name__, template_folder='templates/video_recorder',
+                              static_folder='static/video_recorder')
+
+
+@video_recorder_bp.route('/record-video/<string:project>/<string:split>/<string:label>')
+def record_video(project, split, label):
+    """
+    Display the video recording screen.
+    """
+    project = urllib.parse.unquote(project)
+    split = urllib.parse.unquote(split)
+    label = urllib.parse.unquote(label)
+    path = _lookup_project_path(project)
+    return render_template('video_recording.html', project=project, split=split, label=label, path=path)
+
+
+@video_recorder_bp.route('/save-video/<string:project>/<string:split>/<string:label>', methods=['POST'])
+def save_video(project, split, label):
+    project = urllib.parse.unquote(project)
+    split = urllib.parse.unquote(split)
+    label = urllib.parse.unquote(label)
+    path = _lookup_project_path(project)
+
+    # Read given video to a file
+    input_stream = request.files['video']
+    output_path = os.path.join(path, f'videos_{split}', label)
+    temp_file_name = os.path.join(output_path, 'temp_video.webm')
+    with open(temp_file_name, 'wb') as temp_file:
+        temp_file.write(input_stream.read())
+
+    # Find a video name that is not used yet
+    existing_files = set(glob.glob(os.path.join(output_path, 'video_[0-9]*.mp4')))
+    video_idx = 0
+    output_file = os.path.join(output_path, f'video_{video_idx}.mp4')
+    while output_file in existing_files:
+        video_idx += 1
+        output_file = os.path.join(output_path, f'video_{video_idx}.mp4')
+
+    # Convert video to target frame rate and save to output name
+    subprocess.call(f'ffmpeg -i "{temp_file_name}" -r 30 "{output_file}"', shell=True)
+
+    # Remove temp video file
+    os.remove(temp_file_name)
+
+    return jsonify(success=True)
