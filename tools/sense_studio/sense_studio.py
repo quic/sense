81f41d21b9870010ceb1b6d9908688f5c706fe28
Yashesh Savani <yashesh.savani@twentybn.com>
2021-03-01 11:55:40 -0400

    docstring added

--- tools/sense_studio/sense_studio.py
+++ tools/sense_studio/sense_studio.py
@@ -268,11 +268,13 @@ def add_header(r):
     return r
 
 
-# This context processor will inject a method into templates,
-# Which can be invoked like an ordinary method in HTML templates.
-# E.g. check navigation.html: line 1-2
 @app.context_processor
 def class_labels_processor():
+    """
+    This context processor will inject a method into templates,
+    which can be invoked like an ordinary method in HTML templates.
+    E.g. check navigation.html: line 1-2
+    """
     def inject_class_labels(project):
         path = utils.lookup_project_path(project)
         class_labels = utils.get_class_labels(path)
