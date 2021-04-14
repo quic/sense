02cc40bfe5b00616cd184b9e08d94560f7d91fe1
Yashesh Savani <yashesh.savani@twentybn.com>
2021-04-14 16:42:00 -0300

    replaced signal queue with event

--- sense/controller.py
+++ sense/controller.py
@@ -28,7 +28,7 @@ class Controller:
             path_in: str = Optional[None],
             path_out: str = Optional[None],
             use_gpu: bool = True,
-            signal_queue: Optional[multiprocessing.Queue] = None):
+            signal_event: Optional[multiprocessing.Event] = None):
         """
         :param neural_network:
             The neural network that produces the predictions for the camera image.
@@ -51,8 +51,8 @@ class Controller:
             If provided, store the captured video in a file in this location
         :param use_gpu:
             If True, run the model on the GPU
-        :param signal_queue:
-            Queue for signalling to stop model inference. (If signal = -1, stop inference)
+        :param signal_event:
+            Event for signalling to stop model inference. (If signal = -1, stop inference)
         """
         self.inference_engine = InferenceEngine(neural_network, use_gpu=use_gpu)
         video_source = VideoSource(
@@ -76,7 +76,7 @@ class Controller:
         self.path_out = path_out
         self.video_recorder = None  # created in `display_prediction`
         self.video_recorder_raw = None  # created in `display_prediction`
-        self.signal_queue = signal_queue
+        self.signal_event = signal_event
 
     def run_inference(self):
         runtime_error = None
@@ -125,13 +125,9 @@ class Controller:
                 break
 
             # Press cancel on sense-studio testing page to stop inference
-            if self.signal_queue:
-                try:
-                    if self.signal_queue.get_nowait() == -1:
-                        break
-                except queue.Empty:
-                    # No signal received during the last second
-                    pass
+            if self.signal_event:
+                if self.signal_event.is_set():
+                    break
 
         self._stop_inference()
 
