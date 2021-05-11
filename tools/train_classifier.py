c1b02bbc8759cba721ae3a2ff681727e3aaac3fb
Yashesh Savani <yashesh.savani@twentybn.com>
2021-05-11 19:50:51 -0300

    use classes from project config if config exists

--- tools/train_classifier.py
+++ tools/train_classifier.py
@@ -124,12 +124,17 @@ def train_model(path_in, path_out, model_name, model_version, num_layers_to_fine
     extract_features(path_in, selected_config, backbone_network, num_layers_to_finetune, use_gpu,
                      num_timesteps=num_timesteps, log_fn=log_fn)
 
+    project_config = load_project_config(path_in)
+
     # Find label names
-    label_names = os.listdir(directories.get_videos_dir(path_in, 'train'))
+    if project_config:
+        label_names = project_config['classes'].keys()
+    else:
+        label_names = os.listdir(directories.get_videos_dir(path_in, 'train'))
+
     label_names = natsorted(label_names, alg=ns.IC)
     label_names = [x for x in label_names if not x.startswith('.')]
 
-    project_config = load_project_config(path_in)
     label_names_temporal = ['background']
     if project_config:
         project_tags = project_config['project_tags']
