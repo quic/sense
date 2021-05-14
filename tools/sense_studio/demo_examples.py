089fbe64267fc41fe84c44d99c07719004671595
Yashesh Savani <yashesh.savani@twentybn.com>
2021-05-14 16:08:03 -0300

    updated to demo_examples from demos

--- tools/sense_studio/demo_examples.py
+++ tools/sense_studio/demo_examples.py
@@ -9,15 +9,15 @@ from tools.sense_studio import project_utils
 from tools.sense_studio import socketio
 from tools.sense_studio import utils
 
-demos_bp = Blueprint('demos_bp', __name__)
+demo_examples_bp = Blueprint('demo_examples_bp', __name__)
 
 
-@demos_bp.route('/<string:project>', methods=['GET'])
-def demos_page(project):
+@demo_examples_bp.route('/<string:project>', methods=['GET'])
+def demo_examples_page(project):
     project = urllib.parse.unquote(project)
     path = project_utils.lookup_project_path(project)
     project_config = project_utils.load_project_config(path)
     output_path_prefix = os.path.join(os.path.basename(path), 'checkpoints', '')
-    return render_template('demos.html', project=project, path=path, models=utils.get_available_backbone_models(),
+    return render_template('demo_examples.html', project=project, path=path, models=utils.get_available_backbone_models(),
                            output_path_prefix=output_path_prefix, project_config=project_config)
 
